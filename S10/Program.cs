02ec6923261d6df74d492bd016c5a70331695d5d
tarakavian84@gmail.com <tarakavian84@gmail.com>
2022-05-08 19:40:19 +0430

    Add project files.

---
+++ S10/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter student's score: ");
+            int score = int.Parse(Console.ReadLine());
+            int deducted = 20 - score;
+            Console.WriteLine("The student's deducted score is: " + deducted);
+            Console.ReadKey();
+        }
+    }
+}
